0871b89c9901e10848eee08d8c97a9dd1716f7b7
urbsny <16968591+urbsny@users.noreply.github.com>
2021-02-19 01:58:06 +0900

    IDEヘルパーの導入

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,7 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 // FIXME: サンプルコードです。
 /**
- * @method static Book create(array $all)
+ * App\Models\Book
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $author
+ * @property string|null $release_date
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereAuthor($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereReleaseDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Book whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Book extends Model
 {
