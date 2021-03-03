f20b484c157469179a41729e139eed10ab632795
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-03 18:51:03 +0900

    リファクタリング

--- app/Http/Requests/Sample/BookComment/BookCommentRequest.php
+++ app/Http/Requests/Sample/BookComment/BookCommentRequest.php
@@ -32,6 +32,8 @@ class BookCommentRequest extends FormRequest
 
     protected function prepareForValidation()
     {
+        parent::prepareForValidation();
+
         $book = $this->route('book');
         $comment = $this->route('comment');
         if ($book->id !== $comment->book_id) {
--- app/Http/Requests/Sample/BookComment/BookCommentUpdateRequest.php
+++ app/Http/Requests/Sample/BookComment/BookCommentUpdateRequest.php
@@ -28,7 +28,7 @@ class BookCommentUpdateRequest extends BookCommentRequest
             'choices' => ['nullable', Rule::in(FooBar::all())],
             'description' => ['string'],
             'votes' => ['nullable', 'integer', 'min:1', 'max:5'],
-            'slug' => ['regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/', 'unique:book_comments'],
+            'slug' => ['regex:/^[a-z0-9]+(?:-[a-z0-9]+)*$/', Rule::unique('book_comments')->ignore($this->id)],
             'cover' => ['nullable', 'mimetypes:image/jpeg,image/png,image/bmp', 'max:1024'],
         ];
     }
