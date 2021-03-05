bbe357465eec2e0b02125dc3bf696827b0c1e53b
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-05 21:25:16 +0900

    add scribe

---
+++ app/Console/Commands/Token/CreateTokenCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands\Token;
+
+use Illuminate\Console\Command;
+
+class CreateTokenCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'token:create {email} {password}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create token by email and password';
+
+    private const TOKEN_NAME = 'cli';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        $email = $this->argument("email");
+        $password = $this->argument("password");
+
+        if (!\Auth::attempt(['email' => $email, 'password' => $password])) {
+            $this->error("Unauthenticated.");
+            return 1;
+        }
+
+        $user = \Auth::user();
+        $user->tokens()->where('name', self::TOKEN_NAME)->delete();
+        $token = $user->createToken(self::TOKEN_NAME)->plainTextToken;
+        $this->info($token);
+        return 0;
+    }
+}
--- app/Http/Controllers/StatusController.php
+++ app/Http/Controllers/StatusController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
+/**
+ * Class StatusController
+ * @package App\Http\Controllers
+ * @unauthenticated
+ */
 class StatusController extends Controller
 {
     public function __invoke(Request $request): Response
