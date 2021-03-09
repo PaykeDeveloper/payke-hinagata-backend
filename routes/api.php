bf3dfbf8549d5371932e1e3762299bee3a6de6b4
urbsny <16968591+urbsny@users.noreply.github.com>
2021-03-09 14:55:25 +0900

    リファクタリング

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Sample\BookCommentController;
 use App\Http\Controllers\Sample\BookController;
 use App\Http\Controllers\StatusController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::group(['prefix' => 'v1'], function () {
     Route::get('/status', StatusController::class);
