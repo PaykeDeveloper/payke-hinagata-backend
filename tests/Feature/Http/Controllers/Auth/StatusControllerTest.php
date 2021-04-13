0aa538d5a343a65806dd383dd55f23e912685fb1
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-13 11:35:02 +0900

    招待処理を追加

--- tests/Feature/Http/Controllers/Auth/StatusControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/StatusControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers;
+namespace Tests\Feature\Http\Controllers\Auth;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
--- tests/Feature/Http/Controllers/Auth/TokenControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/TokenControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Http\Controllers;
+namespace Tests\Feature\Http\Controllers\Auth;
 
 use App\Actions\Fortify\CreateNewUser;
 use App\Models\User;
