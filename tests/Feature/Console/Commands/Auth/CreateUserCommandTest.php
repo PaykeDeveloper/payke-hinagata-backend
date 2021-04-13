b9d04d8ed4496f065d1341a4b8ba3ed5370c69f0
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-13 15:13:18 +0900

    招待処理を追加

--- tests/Feature/Console/Commands/Auth/CreateUserCommandTest.php
+++ tests/Feature/Console/Commands/Auth/CreateUserCommandTest.php
@@ -33,7 +33,7 @@ class CreateUserCommandTest extends TestCase
     public function testUserCreateWithoutOptions()
     {
         $email = $this->faker->unique()->email;
-        $password = $this->faker->password;
+        $password = $this->faker->slug;
         $console = $this->artisan('user:create');
         $console->expectsQuestion('What is the name?', $this->faker->userName);
         $console->expectsQuestion('What is the email?', $email);
