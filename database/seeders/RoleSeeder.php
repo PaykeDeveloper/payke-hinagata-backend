c335839b4eb38024ab27c85e6ec03c831c6fbc2e
ameyamashiro <yamashiro7044@gmail.com>
2021-04-14 17:46:02 +0900

    add some roles and its permissions

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,17 +15,70 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
+        $crud_perms = function ($resources) {
+            $perms = [];
+
+            foreach ($resources as $resource) {
+                $perms = array_merge($perms, [
+                    'viewAny_' . $resource,
+                    'view_' . $resource,
+                    'create_' . $resource,
+                    'update_' . $resource,
+                    'delete_' . $resource,
+                ]);
+            }
+            return $perms;
+        };
+
+        $admin_crud_perms = function ($resources) use ($crud_perms) {
+            $perms = [];
+
+            foreach ($resources as $resource) {
+                $perms = array_merge($perms, [
+                    'viewAnyAll_' . $resource,
+                    'viewAll_' . $resource,
+                    'createAll_' . $resource,
+                    'updateAll_' . $resource,
+                    'deleteAll_' . $resource,
+                ]);
+            }
+
+            return $perms;
+        };
+
         $data_set = [
-            'Super Admin',
+            // User Roles
+            [ 'name' => 'Super Admin' ],
+            [ 'name' => 'Admin', 'permissions' => $admin_crud_perms(['user']) ],
+            [ 'name' => 'Manager', 'permissions' => array_merge(
+                $admin_crud_perms(['division', 'member']),
+                ['view_user', 'viewAny_user']
+            )],
+
+            // Member Roles
+            [ 'name' => 'Division Manager', 'permissions' => array_merge(
+                $admin_crud_perms(['project']),
+                ['view_division'],
+            )],
+            [ 'name' => 'Member', 'permissions' => array_merge(
+                $crud_perms(['project']),
+                ['view_division'],
+            )],
         ];
 
         foreach ($data_set as $i => $value) {
-            Role::updateOrCreate([
+            $role = Role::updateOrCreate([
                 'id' => $i + 1,
             ], [
-                'name' => $value,
+                'name' => $value['name'],
                 'guard_name' => 'web',
             ]);
+
+            // 各種パーミッションの割り当
+            $permissions = $value['permissions'] ?? null;
+            if ($permissions) {
+                $role->syncPermissions($permissions);
+            }
         }
     }
 }
