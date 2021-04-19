6d819776528b0dfb2f4459240e5dc9b1ceae4ac3
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-19 20:19:04 +0900

    refactoring

--- app/Models/Traits/HasAuthorization.php
+++ app/Models/Traits/HasAuthorization.php
@@ -9,22 +9,6 @@ trait HasAuthorization
 {
     use HasRoles;
 
-    /**
-     * hasPermissionTo の拡張
-     * {action}_{model} に加えて {action}All_{model} のパーミッションも考慮する
-     */
-    public function hasAllOrPermissionTo(string $action, string $resource): bool
-    {
-        if ($this->hasPermissionTo($action . '_' . $resource)) {
-            // 通常権限
-            return true;
-        } elseif ($this->hasPermissionTo($action . 'All_' . $resource)) {
-            // All 権限
-            return true;
-        }
-        return false;
-    }
-
     private function hasResourcePermissionTo(string $type, string $resource): bool
     {
         return $this->hasPermissionTo(PermissionType::getName($type, $resource));
