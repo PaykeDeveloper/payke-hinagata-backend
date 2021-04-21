038da5e7b555a329055f2318220f6aec565a7194
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-21 12:57:47 +0900

    refactoring

--- tests/Feature/Http/Controllers/Division/MemberControllerTest.php
+++ tests/Feature/Http/Controllers/Division/MemberControllerTest.php
@@ -4,12 +4,13 @@
 
 namespace Tests\Feature\Http\Controllers\Division;
 
-use App\Models\Common\PermissionType;
+use App\Models\Common\UserRole;
 use App\Models\Division\Division;
 use App\Models\Division\Member;
 use App\Models\Division\MemberRole;
 use App\Models\User;
 use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\RefreshSeedDatabase;
 use Tests\TestCase;
 
@@ -24,6 +25,7 @@ class MemberControllerTest extends TestCase
     private User $user;
     private Division $division;
     private Member $member;
+    private Member $target_member;
 
     public function setUp(): void
     {
@@ -34,6 +36,10 @@ class MemberControllerTest extends TestCase
 
         $this->division = Division::factory()->create();
         $this->member = Member::factory()->create([
+            'user_id' => $this->user->id,
+            'division_id' => $this->division->id,
+        ]);
+        $this->target_member = Member::factory()->create([
             'division_id' => $this->division->id,
         ]);
     }
@@ -42,127 +48,96 @@ class MemberControllerTest extends TestCase
      * [正常系]
      */
 
-    public function testIndexSuccessAsUser()
+    /**
+     * @dataProvider provideAuthorizedRole
+     */
+    public function testIndexSuccess($user_role, $member_role)
     {
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Division::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
         $response = $this->getJson(route('divisions.members.index', ['division' => $this->division->id]));
 
         $response->assertOk()
-            ->assertJsonCount(1)
-            ->assertJsonFragment($this->member->toArray());
+            ->assertJsonCount(Member::whereDivisionId($this->division->id)->count())
+            ->assertJsonFragment($this->target_member->toArray());
     }
 
-    public function testShowSuccessAsUser()
+    /**
+     * @dataProvider provideAuthorizedRole
+     */
+    public function testStoreSuccess($user_role, $member_role)
     {
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Division::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
+        $user = User::factory()->create();
+
+        $data = [
+            'user_id' => $user->id,
+            'role_names' => [MemberRole::MEMBER]
+        ];
+
+        $response = $this->postJson(route('divisions.members.store', [
+            'division' => $this->division->id,
+        ]), $data);
+
+        $response->assertOk()
+            ->assertJsonFragment($data);
+    }
+
+    /**
+     * @dataProvider provideAuthorizedRole
+     */
+    public function testShowSuccess($user_role, $member_role)
+    {
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
         $response = $this->getJson(route('divisions.members.show', [
             'division' => $this->division->id,
-            'member' => $this->member->id,
+            'member' => $this->target_member->id,
         ]));
 
         $response->assertOk()
-            ->assertJsonFragment($this->member->toArray());
+            ->assertJsonFragment($this->target_member->toArray());
     }
 
-    public function testUpdateSuccessAsUser()
+    /**
+     * @dataProvider provideAuthorizedRole
+     */
+    public function testUpdateSuccess($user_role, $member_role)
     {
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Division::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Member::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::UPDATE_ALL, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
         $data = ['role_names' => [MemberRole::MEMBER]];
 
         $response = $this->putJson(route('divisions.members.update', [
             'division' => $this->division->id,
-            'member' => $this->member->id,
+            'member' => $this->target_member->id,
         ]), $data);
 
         $response->assertOk()
             ->assertJson($data);
     }
 
-    public function testDestroySuccessAsUser()
+    /**
+     * @dataProvider provideAuthorizedRole
+     */
+    public function testDestroySuccess($user_role, $member_role)
     {
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Division::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::VIEW_ALL, Member::RESOURCE));
-        $this->user->givePermissionTo(PermissionType::getName(PermissionType::DELETE_ALL, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
         $response = $this->deleteJson(route('divisions.members.destroy', [
             'division' => $this->division->id,
-            'member' => $this->member->id,
+            'member' => $this->target_member->id,
         ]));
 
         $response->assertNoContent();
 
-        $result = Member::find($this->member->id);
+        $result = Member::find($this->target_member->id);
         $this->assertNull($result);
-    }
-
-    public function testIndexSuccessUserViewAnyAsMember()
-    {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-
-        $response = $this->getJson(route('divisions.members.index', [
-            'division' => $this->division->id,
-        ]));
-
-        $response->assertOk()
-            ->assertJsonCount(1)
-            ->assertJsonFragment($member->toArray());
-    }
-
-    public function testShowSuccessAsMember()
-    {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-
-        $response = $this->getJson(route('divisions.members.show', [
-            'division' => $this->division->id,
-            'member' => $member->id,
-        ]));
-
-        $response->assertOk()
-            ->assertJson($member->toArray());
-    }
-
-    /**
-     * 更新ができる。
-     */
-    public function testUpdateSuccessAsMember()
-    {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::UPDATE_OWN, Member::RESOURCE));
-
-        $data = ['role_names' => [MemberRole::MEMBER]];
-
-        $response = $this->patchJson(route('divisions.members.update', [
-            'division' => $this->division->id,
-            'member' => $member->id,
-        ]), $data);
-
-        $response->assertOk()
-            ->assertJson($data);
     }
 
     /**
@@ -170,137 +145,124 @@ class MemberControllerTest extends TestCase
      */
 
     /**
-     * 権限の無い別のメンバーにアクセスするとエラーになる。
+     * @dataProvider provideUnAuthorizedRole
      */
-    public function testShowNotFoundAsMember()
+    public function testIndexUnAuthorized($user_role, $member_role)
     {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::UPDATE_OWN, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
-        $response = $this->getJson(route('divisions.members.show', [
-            'division' => $this->division->id,
-            'member' => $this->member->id,
-        ]));
+        $response = $this->getJson(route('divisions.members.index', ['division' => $this->division->id]));
 
         $response->assertNotFound();
     }
 
     /**
-     * viewAny 権限がないとエラー
+     * @dataProvider provideUnAuthorizedRole
      */
-    public function testIndexNotFoundNoPermissionAsMember()
+    public function testStoreUnAuthorized($user_role, $member_role)
     {
-        $response = $this->getJson(route('divisions.members.index', [
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
+        $user = User::factory()->create();
+
+        $data = [
+            'user_id' => $user->id,
+            'role_names' => [MemberRole::MEMBER]
+        ];
+
+        $response = $this->postJson(route('divisions.members.store', [
             'division' => $this->division->id,
-        ]));
-
-        $response->assertNotFound();
-    }
-
-    /**
-     * view 権限がないとエラー
-     */
-    public function testShowNotFoundNoPermissionAsMember()
-    {
-        $response = $this->getJson(route('divisions.members.show', [
-            'division' => $this->division->id,
-            'member' => $this->member->id,
-        ]));
-
-        $response->assertNotFound();
-    }
-
-    /**
-     * update 権限がないとエラー 403
-     */
-    public function testUpdateForbiddenNoPermissionAsMember()
-    {
-        /** @var Member $member */
-        $member = Member::create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-
-        $data = ['role_names' => [MemberRole::MEMBER]];
-
-        $response = $this->patchJson(route('divisions.members.update', [
-            'division' => $this->division->id,
-            'member' => $member->id,
-        ]), $data);
-
-        $response->assertForbidden();
-    }
-
-    /**
-     * destroy 権限がないとエラー 403
-     */
-    public function testDestroyForbiddenNoPermissionAsMember()
-    {
-        /** @var Member $member */
-        $member = Member::create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Member::RESOURCE));
-
-        $response = $this->deleteJson(route('divisions.members.destroy', [
-            'division' => $this->division->id,
-            'member' => $member->id,
-        ]));
-
-        $response->assertForbidden();
-    }
-
-    /**
-     * update はあっても view がない場合はエラー
-     */
-    public function testUpdateForbiddenAsUser()
-    {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::UPDATE_OWN, Member::RESOURCE));
-
-        $data = ['role_names' => [MemberRole::MEMBER]];
-
-        $response = $this->patchJson(route('divisions.members.update', [
-            'division' => $this->division->id,
-            'member' => $member->id,
         ]), $data);
 
         $response->assertNotFound();
     }
 
     /**
-     * destroy はあっても view がない場合はエラー
+     * @dataProvider provideAuthorizedRole
      */
-    public function testDestroyForbiddenAsUser()
+    public function testStoreDuplicated($user_role, $member_role)
     {
-        /** @var Member $member */
-        $member = Member::factory()->create([
-            'user_id' => $this->user->id,
-            'division_id' => $this->division->id,
-        ]);
-        $member->givePermissionTo(PermissionType::getName(PermissionType::VIEW_OWN, Division::RESOURCE));
-        $member->givePermissionTo(PermissionType::getName(PermissionType::UPDATE_OWN, Member::RESOURCE));
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
 
-        $response = $this->deleteJson(route('divisions.members.destroy', [
+        $data = [
+            'user_id' => $this->user->id,
+            'role_names' => [MemberRole::MEMBER]
+        ];
+
+        $response = $this->postJson(route('divisions.members.store', [
             'division' => $this->division->id,
-            'member' => $member->id,
+        ]), $data);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJsonStructure(['errors' => ['user_id']]);
+    }
+
+    /**
+     * @dataProvider provideUnAuthorizedRole
+     */
+    public function testShowUnAuthorized($user_role, $member_role)
+    {
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
+
+        $response = $this->getJson(route('divisions.members.show', [
+            'division' => $this->division->id,
+            'member' => $this->target_member->id,
         ]));
 
         $response->assertNotFound();
+    }
+
+    /**
+     * @dataProvider provideUnAuthorizedRole
+     */
+    public function testUpdateUnAuthorized($user_role, $member_role)
+    {
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
+
+        $data = ['role_names' => [MemberRole::MEMBER]];
+
+        $response = $this->putJson(route('divisions.members.update', [
+            'division' => $this->division->id,
+            'member' => $this->target_member->id,
+        ]), $data);
+
+        $response->assertNotFound();
+    }
+
+    /**
+     * @dataProvider provideUnAuthorizedRole
+     */
+    public function testDestroyUnAuthorized($user_role, $member_role)
+    {
+        $this->user->syncRoles($user_role);
+        $this->member->syncRoles($member_role);
+
+        $response = $this->deleteJson(route('divisions.members.destroy', [
+            'division' => $this->division->id,
+            'member' => $this->target_member->id,
+        ]));
+
+        $response->assertNotFound();
+    }
+
+    public function provideAuthorizedRole(): array
+    {
+        return [
+            [UserRole::ADMINISTRATOR, null],
+            [UserRole::MANAGER, MemberRole::MANAGER],
+            [UserRole::STAFF, MemberRole::MANAGER],
+        ];
+    }
+
+    public function provideUnAuthorizedRole(): array
+    {
+        return [
+            [UserRole::ORGANIZER, MemberRole::MEMBER],
+            [UserRole::STAFF, MemberRole::MEMBER],
+        ];
     }
 }
