7189898fe0ab134f33ca71aed4c7fbac69e50128
urbsny <16968591+urbsny@users.noreply.github.com>
2021-04-22 18:40:50 +0900

    Laravel Lang導入

--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -1,4 +1,13 @@
 <?php
 
 $base = require __DIR__ . '/../../../vendor/laravel-lang/lang/source/validation.php';
-return array_merge($base, []);
+return array_merge($base, [
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+    'attributes' => [
+
+    ],
+]);
