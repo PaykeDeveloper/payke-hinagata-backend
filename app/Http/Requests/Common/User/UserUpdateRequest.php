640602d2de1e6d0b452e14c2e04da31317361cd0
S.U <16968591+urbsny@users.noreply.github.com>
2022-06-15 13:11:46 +0900

    環境最新化

--- app/Http/Requests/Common/User/UserUpdateRequest.php
+++ app/Http/Requests/Common/User/UserUpdateRequest.php
@@ -12,7 +12,7 @@ class UserUpdateRequest extends FormRequest
 {
     public function rules(): array
     {
-        /** @var User $user */
+        /** @var ?User $user */
         $user = $this->route('user');
         return [
             'name' => ['string', 'max:255'],
@@ -25,7 +25,7 @@ class UserUpdateRequest extends FormRequest
             'role_names' => ['array'],
             'role_names.*' => ['string', Rule::exists('roles', 'name')->where(function (Builder $query) {
                 return $query->whereIn('name', UserRole::all());
-            })]
+            })],
         ];
     }
 }
